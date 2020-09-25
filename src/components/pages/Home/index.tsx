71a85187be275ba0fd425e0a1334c93a72e2be31
Thiago Pizzutti <tpizzutti@gmail.com>
2020-09-25 13:45:16 -0300

    feat: Home component and styles done.

--- src/components/pages/Home/index.tsx
+++ src/components/pages/Home/index.tsx
@@ -1,11 +1,23 @@
 import React from 'react'
+import { Title, Form, Container, Choices } from './styles';
 
 const Home = () => {
   return (
-    <div>
+    <>
+      <Title>My Movie Finder</Title>
 
-    </div>
-  )
+      <Container>
+        <Choices>
+          <button type="submit">Movies</button>
+          <button type="submit">Series</button>
+        </Choices>
+        <Form>
+          <input placeholder="Type a movie or a serie name here" />
+          <button type="submit">Search</button>
+        </Form>
+      </Container>
+    </>
+  );
 }
 
 export default Home
