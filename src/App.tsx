f3dd5b12113bd5121abedd023e6756900aab84d8
Thiago Pizzutti <tpizzutti@gmail.com>
2020-09-25 13:45:42 -0300

    chore: import Home and GlobalStyles components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import './App.css';
+import Home from './components/pages/Home';
+import GlobalStyle from './styles/global';
 
 function App() {
   return (
     <div className="App">
-     <h1>My Movie Finder</h1>
+      <Home />
+      <GlobalStyle />
     </div>
   );
 }
