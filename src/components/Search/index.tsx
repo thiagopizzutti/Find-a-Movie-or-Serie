040f467acff80d3bfbd50d434393e55df3dc76a4
Thiago Pizzutti <tpizzutti@gmail.com>
2020-09-26 01:31:47 -0300

    feat: create Search component

---
+++ src/components/Search/index.tsx
@@ -0,0 +1,90 @@
+import React, { useState} from 'react';
+import { useFormik } from 'formik';
+import BounceLoader from 'react-spinners/BounceLoader';
+import * as yup from 'yup';
+import {
+  StyledLoader,
+  ButtonContent,
+  SearchContent,
+  StyledError,
+} from './styles';
+import Form from '../Form';
+import { Input, Button, SelectableButton } from '../';
+import { useData } from '../../hooks/useData';
+
+const Search: React.FC = () => {
+
+  const {
+    isLoading,
+    handleMoviesOrSeries,
+  } = useData()
+
+
+
+  const {
+    handleChange,
+    handleBlur,
+    handleSubmit,
+    values,
+    errors,
+    touched,
+    setFieldValue,
+  } = useFormik({
+    initialValues: {
+      type: '',
+      title: '',
+    },
+    onSubmit:handleMoviesOrSeries,
+      validationSchema: yup.object().shape({
+      title: yup.string().required('Preencha com o nome do filme ou série'),
+    }),
+  });
+
+  const handleType = (type: string) => {
+    setFieldValue('type', type)
+  }
+
+  return (
+    <Form onSubmit={handleSubmit}>
+      <ButtonContent>
+        <SelectableButton
+          type="button"
+          onClick={() => handleType('movie')}
+          isActive={values.type === 'movie'}
+        >
+          Filme
+        </SelectableButton>
+        <SelectableButton
+          type="button"
+          onClick={() => handleType('series')}
+          isActive={values.type === 'series'}
+        >
+          Série
+        </SelectableButton>
+      </ButtonContent>
+      <SearchContent>
+        {isLoading ? (
+          <StyledLoader>
+            <BounceLoader size={60} color="#45aaf2" />
+          </StyledLoader>
+        ) : (
+          <Input
+            onChange={handleChange}
+            onBlur={handleBlur}
+            name="title"
+            type="text"
+            label="Busque o filme ou série"
+            value={values.title}
+          />
+        )}
+        <Button type="submit">Pesquisar</Button>
+      </SearchContent>
+
+      {errors.title && touched.title ? (
+        <StyledError>{errors.title}</StyledError>
+      ) : null}
+    </Form>
+  );
+};
+
+export default Search;
---
+++ src/components/Search/styles.ts
@@ -0,0 +1,62 @@
+import curriedTransparentize from 'polished/lib/color/transparentize';
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  min-width: 300px;
+  margin-top: 30px;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+`;
+
+export const ButtonContent = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+
+  button {
+    width: 370px;
+    margin: 0 0 50px 0;
+  }
+`;
+  export const SearchContent = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    label {
+      font-size: 30px;
+    }
+
+    input {
+      width: 600px;
+      height: 50px;
+      font-size: 20px;
+      margin: 15px 0 20px 0;
+    }
+    button {
+      width: 400px;
+      height: 50px;
+      margin-top: 27px;
+      border-radius: 0 5px 5px 0;
+      :hover {
+        background-color: rgba(26, 100, 156, 0.8);
+        transition: all 0.5s;
+      }
+    }
+  `;
+
+
+
+export const StyledLoader = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+`;
+
+export const StyledError = styled.p`
+    color: #f24545;
+    margin-top: -10px;
+`
+
+
