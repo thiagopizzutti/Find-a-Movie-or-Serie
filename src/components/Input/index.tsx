908a332122b35fb6a295d1356f41ed472c4de005
Thiago Pizzutti <tpizzutti@gmail.com>
2020-09-26 01:30:33 -0300

    feat: create Input component

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,18 @@
+import React, { InputHTMLAttributes } from 'react'
+import { Container } from './styles'
+
+interface IProps extends InputHTMLAttributes<HTMLInputElement>{
+  name: string,
+  label: string
+}
+
+const Input:React.FC<IProps> = ({name, label,...rest}) => {
+  return (
+    <Container>
+      <label htmlFor={name}>{label}</label>
+      <input id={name} {...rest}/>
+    </Container>
+  )
+}
+
+export default Input
---
+++ src/components/Input/styles.ts
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+
+export const Container = styled.header`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  margin: 0 auto;
+
+  label {
+    font-weight: bold;
+    margin-bottom: 5px;
+    font-size: 2rem;
+    color: #939393;
+  }
+  input {
+    width: 100%;
+    font-size: 1.7rem;
+    padding: 8px;
+    margin-bottom: 16px;
+    border: solid 1px #45aaf2;
+    border-radius: 5px 0 0 5px;
+  }
+`;
