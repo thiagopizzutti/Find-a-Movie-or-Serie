74b5937b7d13f477806e502bf4177ac776a60567
Thiago Pizzutti <tpizzutti@gmail.com>
2020-09-28 23:58:59 -0300

    feat: handle 404 error page

--- src/Routes/index.tsx
+++ src/Routes/index.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Home from '../pages/Home';
 import Details from '../pages/Details';
+import Error404 from '../components/Error404';
 
 const Routes: React.FC = () => (
   <Switch>
     <Route exact path="/" component={Home} />
     <Route exact path="/details/:id" component={Details} />
+    <Route path="*" component={Error404} />
   </Switch>
 );
 
