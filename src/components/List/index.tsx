b2ba9868f4e1592d7d8ca536eb869e4752f7562b
Thiago Pizzutti <tpizzutti@gmail.com>
2020-09-29 00:04:07 -0300

    refactor: fix pagination

--- src/components/List/index.tsx
+++ src/components/List/index.tsx
@@ -1,21 +1,25 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { useData } from '../../hooks/useData';
+import Button from '../Button';
 import Card from '../Card';
 
-import { Container } from './styles';
+import { Container, SearchResults, PageControls } from './styles';
 
 const List: React.FC = () => {
-  const { moviesOrSeries } = useData();
+  const { moviesOrSeries, page, handlePage, totalPages } = useData();
 
   return (
     <>
-      {!!moviesOrSeries.data.totalResults && (
-        <h1>
-          Sua busca resultou em:{' '}
-          <strong>{moviesOrSeries.data.totalResults}</strong>
-        </h1>
-      )}
+      <SearchResults>
+        {!!moviesOrSeries.data.totalResults && (
+          <h1>
+            Sua busca resultou em: ({' '}
+            <strong>{moviesOrSeries.data.totalResults}</strong>resultados)
+          </h1>
+        )}
+      </SearchResults>
+
       <Container>
         {moviesOrSeries.error ? (
           <p>{moviesOrSeries.error}</p>
@@ -26,6 +30,14 @@ const List: React.FC = () => {
             </Link>
           ))
         )}
+        <PageControls>
+          {page > 1 && (
+            <Button onClick={() => handlePage('-')}>anterior</Button>
+          )}
+          {moviesOrSeries.data.items.length !== 0 && page !== totalPages && (
+            <Button onClick={() => handlePage('+')}>próximo</Button>
+          )}
+        </PageControls>
       </Container>
     </>
   );
